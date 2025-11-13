edbbb3f93af452b47f7edecfef856712aed473e8
Patrick Karlsson <Patrick13karlsson@gmail.com>
2025-11-13 15:37:49 +0100

    PlaceholderKlasser
    Placeholderklasser för Podcast och Avsnitt

---
+++ PoddProjektV3.2/PoddProjektV3.2/Models/Avsnitt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoddProjektV3._2.Models
+{
+    public class Avsnitt
+    {
+        public string Titel { get; set; }
+        public string Beskrivning { get; set; }
+        public string PremiarDatum { get; set; }
+        public Avsnitt() {}
+
+    }
+}
