6642b5bb85c7c5bc33b145ea3609d0ba51de291d
FridaSel <frida.selin@gmail.com>
2025-11-13 15:48:47 +0100

    Podcast

--- PoddProjektV3.2/PoddProjektV3.2/Models/Podcast.cs
+++ PoddProjektV3.2/PoddProjektV3.2/Models/Podcast.cs
@@ -8,7 +8,9 @@ namespace PoddProjektV3._2.Models
 {
     public class Podcast
     {
-        public string Namn { get; set; }
+        public string Titel { get; set; }
+        public string Beskrivning { get; set; }
+        public string? Kategori { get; set; }
         public List<Avsnitt> PoddAvsnitt { get; set; }
         public Podcast() { }
     }
