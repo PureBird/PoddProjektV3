4f05eb8c5be63d2385a5c4098e9276d12f6148ba
FridaSel <frida.selin@gmail.com>
2025-11-13 16:12:51 +0100

    tillagd Kategori klass i models

---
+++ PoddProjektV3.2/PoddProjektV3.2/Models/Kategori.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoddProjektV3._2.Models
+{
+    internal class Kategori
+    {
+        public List<Podcast> PoddLista { get; set; }
+
+    }
+}
