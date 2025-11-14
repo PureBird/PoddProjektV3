37bf47d4c550a1865c843c39388d513211ef7b39
FridaSel <frida.selin@gmail.com>
2025-11-14 15:42:21 +0100

    Meny och Register fönster tillagda

---
+++ PoddProjektV3.2/PoddProjektV3.2/Meny.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace PoddProjektV3._2
+{
+    partial class Meny
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Register = new Button();
+            Sök = new Button();
+            Kategori = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            Register.Location = new Point(69, 49);
+            Register.Name = "button1";
+            Register.Size = new Size(229, 65);
+            Register.TabIndex = 0;
+            Register.Text = "button1";
+            Register.UseVisualStyleBackColor = true;
+            Register.Click += button1_Click;
+            // 
+            // button2
+            // 
+            Sök.Location = new Point(69, 143);
+            Sök.Name = "button2";
+            Sök.Size = new Size(229, 65);
+            Sök.TabIndex = 1;
+            Sök.Text = "button2";
+            Sök.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            Kategori.Location = new Point(69, 236);
+            Kategori.Name = "button3";
+            Kategori.Size = new Size(229, 65);
+            Kategori.TabIndex = 2;
+            Kategori.Text = "button3";
+            Kategori.UseVisualStyleBackColor = true;
+            // 
+            // Meny
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(Kategori);
+            Controls.Add(Sök);
+            Controls.Add(Register);
+            Name = "Meny";
+            Text = "Meny";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button Register;
+        private Button Sök;
+        private Button Kategori;
+    }
+}
---
+++ PoddProjektV3.2/PoddProjektV3.2/Register.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace PoddProjektV3._2
+{
+    partial class Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            header = new Label();
+            comboBoxFilter = new ComboBox();
+            dataGridView1 = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // header
+            // 
+            header.AutoSize = true;
+            header.Font = new Font("Stencil", 26F, FontStyle.Bold | FontStyle.Italic);
+            header.Location = new Point(58, 27);
+            header.Name = "header";
+            header.Size = new Size(239, 52);
+            header.TabIndex = 0;
+            header.Text = "Register";
+            // 
+            // comboBoxFilter
+            // 
+            comboBoxFilter.FormattingEnabled = true;
+            comboBoxFilter.Location = new Point(66, 82);
+            comboBoxFilter.Name = "comboBoxFilter";
+            comboBoxFilter.Size = new Size(300, 28);
+            comboBoxFilter.TabIndex = 1;
+            comboBoxFilter.Text = "Alla Poddar";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3 });
+            dataGridView1.Location = new Point(66, 146);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(610, 270);
+            dataGridView1.TabIndex = 2;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "Podcast";
+            Column1.MinimumWidth = 6;
+            Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "Beskrivning";
+            Column2.MinimumWidth = 6;
+            Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "Antal avsnitt";
+            Column3.MinimumWidth = 6;
+            Column3.Name = "Column3";
+            // 
+            // Register
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(comboBoxFilter);
+            Controls.Add(header);
+            Name = "Register";
+            Text = "Register";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label header;
+        private ComboBox comboBoxFilter;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+    }
+}
